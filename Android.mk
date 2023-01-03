0e63cca6dfaad06c4d80b0ca1cd7025a2ea838da
ragarcia87 <70825623+juic3b0x@users.noreply.github.com>
2023-01-02 20:58:14 -0600

    Revert
    Revert old repo, branch off for development code...

--- Android.mk
+++ Android.mk
@@ -7,6 +7,8 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE), milanf)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ifeq ($(TARGET_DEVICE), denver)
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
 endif
--- twrp_denver.mk
+++ twrp_denver.mk
@@ -5,29 +5,12 @@
 # Copyright (C) 2022-juic3b0x
 #
 
-# Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Shipping API Level
-PRODUCT_SHIPPING_API_LEVEL := 31
-
 # VNDK Level
 PRODUCT_TARGET_VNDK_VERSION := 31
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from denver device
 $(call inherit-product, device/motorola/denver/device.mk)
@@ -35,12 +18,9 @@ $(call inherit-product, device/motorola/denver/device.mk)
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := denver
 PRODUCT_NAME := twrp_denver
-PRODUCT_BRAND := Moto
-PRODUCT_MODEL := XT2131-1
-PRODUCT_MANUFACTURER := Motorola
-PRODUCT_RELEASE_NAME := Moto G Sylus 5G
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g stylus 5G
+PRODUCT_MANUFACTURER := motorola
